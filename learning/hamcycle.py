7feb8accb354779aea7caf56d88d94f94b4169f8
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2023-03-21 13:31:52 +0530

    Hamiltonian

--- learning/hamcycle.py
+++ learning/hamcycle.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def isPrime(n: int) -> bool:
     """This function is to determine whether or not the given number is a prime number"""
 
@@ -52,19 +51,24 @@ class Graph():
         self.graph = [[0 for column in range(vertices)]
                       for row in range(vertices)]
         self.V = vertices
+        #2 circular arrays of vertices are created
         self.vertices_i = list(range(0,self.V//2))
         self.vertices_j = list(range(self.V//2,self.V))
 
-    def isSafe(self, v, pos, path,diff):
+    def isSafe(self, v:int, pos:int, path:list,diff:int)->bool:
 
-        if self.graph[path[pos-1]][v] == 0:
+        """The above function checks if the next vertex is valid and safe to travel to"""
+
+        if self.graph[path[pos-1]][v] == 0: #Checks if the graph is connected
             return False
-
-        for vertex in path:
+        
+        for vertex in path: #Checks if the vertex is not repeated
             if vertex == v:
                 return False
         
-        if pos > 1:
+        if pos > 1:        
+            #Checks if the difference is valid
+            #2 circular array of vertices is created, and difference is added and checked if the next element is valid.
             if v >= self.V//2:
                 index = self.vertices_j.index(path[pos-2])
                 count = (index + diff) % len(self.vertices_j)
@@ -83,9 +87,10 @@ class Graph():
     
         return True
 
-    def hamCycleUtil(self, path, pos,diff):
+    def hamCycleUtil(self, path:list, pos:int,diff:int,size:int,file)->bool:
 
-        if pos == self.V:
+        """The function is used to traverse the vertices of the graph"""
+        if pos == size:
 
             if self.graph[path[pos-1]][path[0]] == 1:
                 self.graph[path[pos-1]][path[0]] = 0
@@ -98,61 +103,110 @@ class Graph():
 
             if self.isSafe(v, pos, path,diff) == True:
 
+                #As the traversal is edge destroying changes are made to adjacency matrix
                 path[pos] = v
                 self.graph[path[pos-1]][v] = 0
                 self.graph[v][path[pos-1]] = 0
 
-                if self.hamCycleUtil(path, pos+1,diff) == True:
+                if self.hamCycleUtil(path, pos+1,diff,size,file) == True:
                     return True
 
+                #If the vertex was not appropriate the changes are removed
                 self.graph[path[pos-1]][v] = 1
                 self.graph[v][path[pos-1]] = 1
                 path[pos] = -1
-            #print(path)
         return False
 
-    def hamCycle(self,diff):
-        path = [-1] * self.V
+    def hamCycle(self,diff:int,size:int,file,start:int)->bool:
 
-        path[0] = 0
+        """The function finds one Hamiltonian cycle or normal cycle in the graph based on difference and size"""
+        path = [-1] * size
 
-        if self.hamCycleUtil(path, 1,diff) == False:
-            #print("Solution does not exist\n")
+        path[0] = start
+
+        if self.hamCycleUtil(path, 1,diff,size,file) == False:
             return False
 
-        self.printSolution(path,diff)
+        self.printSolution(path,diff,file)
         return True
 
-    def printSolution(self, path,diff):
-        #print("Solution Exists: Following is one Hamiltonian Cycle")
-        print("The difference should be {}".format(diff))
-        for vertex in path:
-            print(vertex+1, end=" ")
-        print(path[0]+1)
+    def printSolution(self, path:list,diff:int,file)->None:
+        #1 is added to all vertices as the expected output varies from 1 to n.
 
-    def numHamCycles(self):
-        count = 0
+        print("The difference is {}".format(diff),file=file)
+        for vertex in path:
+            print(vertex+1, end=" ",file=file)
+        print(path[0]+1,file=file)
+
+    def numHamCycles(self,size:int,file)->int:
+
+        """The function is used to find Hamiltonian or normal cycles based on difference, size and start"""
+        count = 0 #The index in the prime list
+        number = 0 #The number of cycles in the graph
+        #The difference varies from 1,2,3...primes upto pi(n) - 2
         prime_list = pi_list(self.V//2)
+        prime_list = [x for x in prime_list if x <= prime_list[len(prime_list)-1]-2]
         prime_list.insert(0,1)
         while 1 in self.graph[0]:
-            #print(self.graph)
-            if self.hamCycle(prime_list[count]) == False:
+            start = 0
+            if count>= len(prime_list):
                 break
-            else:
+            if self.hamCycle(prime_list[count],size,file,start) == False:
+                if size == self.V//2:
+                    #As the cycles can start from any vertex, for normal cycles
+                    #This tries every combination of starting vertex for given difference if we are checking for normal cycles
+                    while(start<size):
+                        start = start + 1
+                        while self.hamCycle(prime_list[count],size,file,start) == True:
+                           number = number + 1
                 count = count + 1
-        print("The number of disjoint edges is {}".format(self.disjoint()))
-        return count
+                continue
+            else:
+                number = number + 1
+                if size == self.V//2:
+                    #Similar to above we are using this to check every starting vertex for the normal cycle
+                    while(start<size):
+                       while self.hamCycle(prime_list[count],size,file,start) == True:
+                           number = number + 1
+                       start = start + 1
+        return number
 
     def disjoint(self):
+        """This function is used to count the disjoint vertices left after all the cycles are done"""
         disjoint = 0
+        #The number of 1's left in the adjacency matrix after all the cycles
         for i in range(len(self.graph)):
             for j in range(len(self.graph[i])):
                 if self.graph[i][j] == 1:
                     disjoint = disjoint + 1
         return disjoint//2
+    
+    def details(self,file):
+
+        """The function used to list all the Hamiltonian cycles,normal cycles and the disjoint edges in a graph"""
+
+        print("The Hamiltonian cycles start here:",file=file)
+        print("The number of hamiltonian cycles is {}".format(self.numHamCycles(self.V,file)),file=file)
+        print(file=file)
+        print("The cycles of size {} start here: ".format(self.V//2),file=file)
+        print("The number of cycles with size {} is {}".format(self.V//2,self.numHamCycles(self.V//2,file)),file=file)
+        print(file=file)
+        print("The number of disjoint edges is {}".format(self.disjoint()),file=file)
+        print("\n",file=file)
 
 if __name__ == "__main__":
-    n = int(input("Enter the value of n: "))
-    g1 = Graph(2*n)
-    g1.graph = knodel_adjacency(n)
-    print("The number of cycles is {}".format(g1.numHamCycles()))
+    n1 = int(input("Enter the lower bound of n: "))
+    n2 = int(input("Enter the upper bound of n: "))
+    file = open("Hamiltonian.txt","w")
+
+    for i in range(n1,n2+1):
+        #Loop to iterate from lower to upper index to check for all graphs listed in the range
+        g1 = Graph(2*i)
+        g1.graph = knodel_adjacency(i)
+        print("The value of n is {}".format(i),file=file)
+        print("The adjacency matrix (C) is:",file=file)
+        print(g1.graph[:(g1.V//2),(g1.V//2):],file=file)
+        print(file=file)
+        g1.details(file)
+
+    file.close()
